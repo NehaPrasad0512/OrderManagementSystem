222ae0789e1330c9fef8fb0c0623a7de8f510d04
sher <sher@white_devil>
2021-05-18 15:12:42 +0530

    COMMIT

--- OrderMS/src/main/java/com/group7/order/orderdetails/OrderDetailsApplication.java
+++ OrderMS/src/main/java/com/group7/order/orderdetails/OrderDetailsApplication.java
@@ -2,9 +2,11 @@ package com.group7.order.orderdetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //commiting
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderDetailsApplication {
 	
 	public static void main(String[] args) {
--- ProductMS/src/main/java/com/group7/product/productdetails/ProductDetailsApplication.java
+++ ProductMS/src/main/java/com/group7/product/productdetails/ProductDetailsApplication.java
@@ -2,9 +2,11 @@ package com.group7.product.productdetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //Main Application of Product Details
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductDetailsApplication {
 	
 	public static void main(String[] args) {

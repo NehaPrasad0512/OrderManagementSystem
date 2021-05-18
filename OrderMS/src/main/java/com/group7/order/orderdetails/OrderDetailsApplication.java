906c9ba885227199421d345fa7b45fd631632c1e
NehaPrasad0512 <nehaprasad2912@gmal.com>
2021-05-18 08:56:24 +0530

    commiting the main application of orderms

--- OrderMS/src/main/java/com/group7/order/orderdetails/OrderDetailsApplication.java
+++ OrderMS/src/main/java/com/group7/order/orderdetails/OrderDetailsApplication.java
@@ -1,9 +1,15 @@
 package com.group7.order.orderdetails;
 
-public class OrderDetailsApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
+public class OrderDetailsApplication {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SpringApplication.run(OrderDetailsApplication.class, args);
 
 	}
 
